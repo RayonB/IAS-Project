478b4492a53b86e5afb339fc353037897a63f684
RayonB <rinevarayon@gmail.com>
2025-04-09 17:34:49 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IAS-Project',
+  webDir: 'dist'
+};
+
+export default config;
