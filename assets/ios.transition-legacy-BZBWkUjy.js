9c7afdb3e3d016ac230fce611e98fdb312e5a551
sheykisses <dancilhershey65@gmail.com>
2025-04-21 09:06:31 +0800

    Updates

--- assets/ios.transition-legacy-BZBWkUjy.js
+++ assets/ios.transition-legacy-BZBWkUjy.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B81nt51l.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-BQsGvsgx.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
